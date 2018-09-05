ff68268b811696e36c237d25ff097177f94b708b
LHWen <1109070804@qq.com>
2018-09-05 18:02:36 +0800

    Math Class

---
+++ src/MathClass.java
@@ -0,0 +1,12 @@
+public class MathClass {
+
+    public static void main (String []args) {
+
+        System.out.println("90 度的正弦值：" + Math.sin(Math.PI/2));
+        System.out.println("0 度的余弦值：" + Math.cos(0));
+        System.out.println("60 度的正切值：" + Math.tan(Math.PI/3));
+        System.out.println("1 的反正切值： " + Math.atan(1));
+        System.out.println("π/2 的角度值：" + Math.toDegrees(Math.PI/2));
+        System.out.println(Math.PI);
+    }
+}
